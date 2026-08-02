9803a806e5bde9b6b63061f2c2e5829ac9eabfca
ArielEskin <arieleskin360@gmail.com>
2026-08-02 22:26:47 +0300

    .

--- MonoGame2026_Heb-main/MonoGame2026_Heb/obj/Debug/net9.0/MonoGame2026_Heb.AssemblyInfo.cs
+++ MonoGame2026_Heb-main/MonoGame2026_Heb/obj/Debug/net9.0/MonoGame2026_Heb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MonoGame2026_Heb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+258d3a142a3f00f2e7adc4ab4169455aa84a9153")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1903dff12f8611e2a0e189af97ba268fc05f2425")]
 [assembly: System.Reflection.AssemblyProductAttribute("MonoGame2026_Heb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MonoGame2026_Heb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
